ff8b043b49f1ac01053155f005714894bec10bda
Thomas Goodwin <thomas.gabriel.goodwin@gmail.com>
2022-02-03 19:37:15 -0600

    Adding missing integration tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -11,4 +11,28 @@ RSpec.describe 'Creating a book', type: :feature do
     visit books_path
     expect(page).to have_content('harry potter')
   end
+
+  scenario 'No title' do
+    visit new_book_path
+    fill_in 'Author', with: 'J. K. Rowling'
+    fill_in 'Price', with: 19.99
+    click_on 'Create Book'
+    expect(page).to have_content('error')
+  end
+
+  scenario 'No author' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Price', with: 19.99
+    click_on 'Create Book'
+    expect(page).to have_content('error')
+  end
+
+  scenario 'No price' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J. K. Rowling'
+    click_on 'Create Book'
+    expect(page).to have_content('error')
+  end
 end
