150c40088580e9b3c4e199e3a5d8341ad2518b44
Thomas Goodwin <thomas.gabriel.goodwin@gmail.com>
2022-02-10 10:21:27 -0600

    Adding users and user_books
    Linking between indeces

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+    has_many :users
+    has_many :user_books
     validates :title, presence: true
     validates :author, presence: true
     validates :price, presence: true
